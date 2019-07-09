9e7180b42427d5ab03b23bd4230e76ace0f546e0
Andrew Cowie <andrew@operationaldynamics.com>
2019-07-09 21:36:28 +1000

    Use fully qualified paths

--- build_everything.sh
+++ build_everything.sh
@@ -1,1 +1,9 @@
-ionice -c 3 nice stack build --test --no-run-tests --haddock --haddock-deps --only-dependencies
+ionice -c 3 nice stack build \
+	--test --no-run-tests \
+	--bench --no-run-benchmarks \
+	--haddock --haddock-deps \
+	--no-haddock-hyperlink-source \
+	--no-haddock-internal \
+	--haddock-arguments "--no-print-missing-docs" \
+	--only-dependencies
+
