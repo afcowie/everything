c777bdb508ddefe5da9cc718ebd4bdb583da80fd
Andrew Cowie <andrew@operationaldynamics.com>
2019-11-26 06:23:31 +1100

    Bump resolver to lts-14.15

--- serve_documentation.sh
+++ serve_documentation.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+set -e
+
 rm -rf .stack-work/hoogle/
-ionice -c 3 nice stack hoogle --no-setup --rebuild
+ionice -c 3 nice stack hoogle --setup --rebuild
 ionice -c 3 nice hoogle server --local --database=`find .stack-work/hoogle/ -name database.hoo` 
